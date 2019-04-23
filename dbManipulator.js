bf383383d5d6812d14d472d995d82702aec03bf4
Milky <48883085+ilan-codefresh@users.noreply.github.com>
2019-04-23 17:01:30 +0300

    Update dbManipulator.js

--- dbManipulator.js
+++ dbManipulator.js
@@ -4,7 +4,7 @@ const mysql = require('promise-mysql');
 const debug = require('debug')('DB');
 
 let _connection;
-let _host = "";
+var _host = "";
 var _user = "";
 var _password = "";
 var _dbName = "";
